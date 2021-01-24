adfb912ad11d3f0314906545e3dd2657c2fa892d
Adrian Sadłocha <adrian@sadlocha.eu>
2021-01-24 12:51:54 +0100

    Add initial version
    The main bot instance is called "Korova Milky Bot" as a tribute to the
    Anthony Burgess's novel titled "A Clockwork Orange". Hence the project
    name, "korova".

---
+++ src/main.rs
@@ -0,0 +1,144 @@
+use std::collections::HashMap;
+use std::env;
+use std::sync::Arc;
+
+use serenity::model::id::ChannelId;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready, user::User},
+    prelude::*,
+};
+
+static ENVIRONMENT_VARIABLE_NAME: &str = "KOROVA_TOKEN";
+static MIN_PLAYERS: usize = 2;
+
+struct Handler;
+
+struct ChannelGather {
+    players: Vec<User>,
+}
+
+impl ChannelGather {
+    fn new() -> Self {
+        ChannelGather { players: vec![] }
+    }
+
+    fn add(&mut self, player: &User) {
+        if self.players.iter().map(|p| p.id).all(|id| id != player.id) {
+            self.players.push(player.clone());
+        }
+    }
+
+    fn del(&mut self, player: &User) {
+        self.players.retain(|p| p.id != player.id);
+    }
+
+    fn play(&mut self) -> String {
+        if self.players.len() < MIN_PLAYERS {
+            String::from("We need at least 2 players.")
+        } else {
+            let lines = vec![
+                String::from("Get ready for the game. Let me summon everyone:"),
+                self.players
+                    .iter()
+                    .map(|p| p.mention().to_string())
+                    .collect::<Vec<_>>()
+                    .join(" | "),
+                String::from("Good luck & have fun!"),
+            ];
+            self.players = vec![];
+            lines.join("\n")
+        }
+    }
+
+    fn status(&self) -> String {
+        if self.players.is_empty() {
+            String::from("Nobody wants to play right now. Write `!add` to join.")
+        } else {
+            let mut lines = vec![
+                String::from("Ready players:"),
+                self.players
+                    .iter()
+                    .map(|p| p.name.clone())
+                    .collect::<Vec<String>>()
+                    .join(" | "),
+            ];
+            if self.players.len() >= MIN_PLAYERS {
+                lines.push(String::from("Write `!play` to start the game."));
+            }
+            lines.join("\n")
+        }
+    }
+}
+
+struct GlobalGather;
+
+impl TypeMapKey for GlobalGather {
+    type Value = Arc<RwLock<HashMap<ChannelId, ChannelGather>>>;
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let type_map = ctx.data.read().await;
+        let lock = type_map.get::<GlobalGather>().unwrap().clone();
+        let mut map = lock.write().await;
+        let gather = map.entry(msg.channel_id).or_insert_with(ChannelGather::new);
+
+        let response: Option<String> = match &msg.content[..] {
+            "!add" => {
+                gather.add(&msg.author);
+                Some(gather.status())
+            }
+            "!del" => {
+                gather.del(&msg.author);
+                Some(gather.status())
+            }
+            "!play" => Some(gather.play()),
+            "!status" => Some(gather.status()),
+            "!help" => {
+                let lines = vec![
+                    "Gather commands: `!add`, `!del`, `!play`, `!status`.",
+                    "Misc. commands: `!help`, `!ping`.",
+                ];
+                Some(lines.join("\n"))
+            }
+            "!ping" => Some(format!("Pong, {}.", msg.author.mention())),
+            _ => None,
+        };
+
+        if let Some(r) = response {
+            if let Err(e) = msg.channel_id.say(&ctx.http, r).await {
+                eprintln!("Error sending message: {:?}", e);
+            }
+        };
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        eprintln!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var(ENVIRONMENT_VARIABLE_NAME).unwrap_or_else(|_| {
+        panic!(
+            "Missing Discord bot token in {} environment variable.",
+            ENVIRONMENT_VARIABLE_NAME
+        )
+    });
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<GlobalGather>(Arc::new(RwLock::new(HashMap::new())));
+    }
+
+    if let Err(e) = client.start().await {
+        eprintln!("Client error: {:?}", e);
+    }
+}
